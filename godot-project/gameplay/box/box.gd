6b24510cb9e3ba52ec9ed93cc4a19c503d5084ff
Moisés J <moisesjbc@gmail.com>
2023-04-29 15:11:28 +0100

    Add box atachment

--- godot-project/gameplay/box/box.gd
+++ godot-project/gameplay/box/box.gd
@@ -8,8 +8,8 @@ var near_magnets = []
 
 func _ready():
 	set_color(Color.orange)
-	
-	
+
+
 func set_color(new_color):
 	color = new_color
 	$sprite.modulate = color
@@ -21,4 +21,11 @@ func _process(delta):
 	for near_magnet in near_magnets:
 		velocity += (near_magnet.global_position - global_position).normalized() * (near_magnet.radius() - global_position.distance_to(near_magnet.global_position))
 		
-	move_and_collide(velocity * delta)
+	var collision = move_and_collide(velocity * delta)
+	if collision and collision.collider.name == "player":
+		var position = global_position
+		get_parent().remove_child(self)
+		collision.collider.get_node("attachments").add_child(self)
+		set_process(false)
+		set_collision_layer_bit(0, false)
+		global_position = position
