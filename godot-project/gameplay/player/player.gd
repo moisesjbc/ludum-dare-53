0fd0a98cded8c415b36f092da83296832d6f5c2e
Moisés J <moisesjbc@gmail.com>
2023-04-29 12:48:48 +0100

    Add player movement

---
+++ godot-project/gameplay/player/player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+
+export (int) var speed = 500
+
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -1
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = 1
+		
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -1
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = 1
+		
+	move_and_collide(speed * velocity * delta)
+	
+	look_at(get_global_mouse_position())
